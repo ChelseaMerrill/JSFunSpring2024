d8631eb1e81886a4f81a974dd2410a5d92ba0631
Chelsea Merrill <chelmer1223@aol.com>
2024-03-20 18:51:28 -0400

    started game

--- exercises/14-more-control-flow/01-ternary-operator.js
+++ exercises/14-more-control-flow/01-ternary-operator.js
@@ -13,3 +13,5 @@ let num = 9; // e.g.
 // WRITE YOUR ANSWER BELOW THIS LINE
 let isEvenOrOdd = num % 2 === 0 ?  'even' : 'odd';
 console.log(isEvenOrOdd);
+
+
---
+++ exercises/15-guessing-game/index.js
@@ -0,0 +1,8 @@
+import  prompt  from "readline-sync";
+const num = prompt.question("please guess the number");
+console.log(num);
+
+//create a number at random (let or var)
+//check that the number is between 1 and 10 (otherwise tell them to enter a number between 1 and 10)
+//Tell the user if their guess is too high, too low, or correct
+//Game over if the guess correctly (success message)
