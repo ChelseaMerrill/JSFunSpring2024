b24aff65f616685b3ccc56c44f5420f30bf2733c
Chelsea Merrill <chelmer1223@aol.com>
2024-04-17 20:42:25 -0400

    class ex

--- exercises/22-transforming-dom/transforming-dom.js
+++ exercises/22-transforming-dom/transforming-dom.js
@@ -5,4 +5,27 @@
  */
 (function () {
   // Put your answers in here
+
+  //1
+
+  const newlink = document.querySelector('.alert-warning')
+  newlink.href="https://developer.mozilla.org/en-US/docs/Web/JavaScript";
+
+  const newText = document.querySelector('.alert-success');
+  newText.innerHTML = "I am victorious!";
+
+  const newBgColor = document.querySelector(".alert-info");
+  newBgColor.style.background = "pink";
+
+  const txtColor = document.querySelector('#change-color');
+  txtColor.classList.add("text-white");
+
+  const mybutton = document.querySelector("#my-button");
+  const textDarkDiv = document.querySelector("#second-button");
+  if(mybutton.classList.contains("btn-primary")){
+    textDarkDiv.textContent = " blue";
+
+  }
+
+
 })();
