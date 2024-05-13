0cc844b55410c5a4a2a0856d7bda323fb72ec6e1
Chelsea Merrill <chelmer1223@aol.com>
2024-05-13 09:21:35 -0400

    mondays hw

--- exercises/33-ajax-series/ajax-series.js
+++ exercises/33-ajax-series/ajax-series.js
@@ -17,4 +17,49 @@
    *
    * You must make two AJAX request to solve this problem.
    */
+
+const fetchCharacters = () => {
+  return axios.get('https://rickandmortyapi.com/api/character')
+    .then(response => response.data.results)
+    .catch(error => console.error('Error fetching characters:', error));
+};
+
+// AJAX request to fetch individual character by ID
+const fetchCharacterById = (id) => {
+  return axios.get(`https://rickandmortyapi.com/api/character/${id}`)
+    .then(response => response.data)
+    .catch(error => console.error('Error fetching character by ID:', error));
+};
+
+// Populate dropdown with character options
+const populateDropdown = (characters) => {
+  const dropdown = document.getElementById('dropdown');
+  characters.forEach(character => {
+    const option = document.createElement('option');
+    option.text = character.name;
+    option.value = character.id;
+    dropdown.appendChild(option);
+  });
+};
+
+// Update image and caption when a character is selected
+const updateCharacterInfo = (character) => {
+  const img = document.getElementById('get-schwifty');
+  const caption = document.getElementById('photo-caption');
+  img.src = character.image;
+  img.alt = character.name;
+  caption.innerHTML = `<em>${character.name}</em>`;
+};
+
+// Event listener for dropdown change
+document.getElementById('dropdown').addEventListener('change', function() {
+  const characterId = this.value;
+  if (characterId !== "Select a character") {
+    fetchCharacterById(characterId)
+      .then(character => updateCharacterInfo(character));
+  }
+});
+
+fetchCharacters()
+  .then(characters => populateDropdown(characters));
 })();
